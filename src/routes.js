aebcb6e573c662d8a6edc53e11ca346a40e0e525
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-09-02 22:49:54 -0300

    Configure sucrase with nodemon to allow newer JS syntax

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require('express')
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json('Hello World')
+  return res.json('Hello World');
 })
 
-module.exports = routes;
+export default routes;
