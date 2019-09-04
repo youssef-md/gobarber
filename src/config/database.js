98f44d6d5014908c9e01fc1661c9b963f96d9483
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-09-03 23:13:56 -0300

    Folder structure for the app, configure paths and credentials for sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'chavoso',
+  database: 'chavoso',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
