ea6dc6174e279d010ed00fcf9e5876a75a63d5ca
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-09-04 20:43:05 -0300

    Create the connection to the postgres and the models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json('Hello World');
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Youssef Muhadm',
+    email: 'youssef@gmail.com',
+    password_hash: '123131323',
+  });
+  return res.json(user);
 });
 
 export default routes;
