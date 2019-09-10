24872c60b6128002fcfce5b7a31980c3ca29bce7
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-09-10 17:15:42 -0300

    Store method in the file controller

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -14,8 +15,6 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
