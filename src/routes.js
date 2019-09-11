9e69d4a947bd663ca2da9109c38af080b665c86d
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-09-11 00:31:56 -0300

    ProviderController to list all providers with theirs avatar relashionship

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -12,8 +13,10 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-routes.use(authMiddleware);
+routes.use(authMiddleware); // global middleware for the routes bellow
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
