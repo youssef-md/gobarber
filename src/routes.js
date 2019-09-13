384c9daa0d6efdf3c26ab43b0889daf2731ac424
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-09-13 18:39:16 -0300

    Marking a notification as read via PUT

--- src/routes.js
+++ src/routes.js
@@ -32,5 +32,6 @@ routes.post('/appointments', AppointmentController.store);
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 export default routes;
