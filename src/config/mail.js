d79282c8837956a38a97db902631243d464ec4c9
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-09-14 13:32:08 -0300

    Config MailTrap to send an email in dev mode

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'f2eef4bb3482b4',
+    pass: '2544ef92eb9706',
+  },
+  secure: false,
+  default: {
+    from: 'Equipe Chavoso <noreply@chavoso.com>',
+  },
+};
