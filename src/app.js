6a6502d5b97d229d2627b944c4b3fec3428a4433
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-09-27 13:58:34 -0300

    Initi sentry in this app

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import { resolve } from 'path';
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
+
 import routes from './routes';
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -20,6 +26,7 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://4e0eedfef2ac486297e09b987dfa7e6d@sentry.io/1764026',
+};
