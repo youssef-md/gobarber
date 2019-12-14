905bde78e760287a49a309e0cf8dab1faf7cacbf
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2019-12-13 23:59:19 -0300

    Config Eslint and Prettier to this project with AirBnB pattern

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-    'prettier',
-  ],
+  extends: ['airbnb-base', 'prettier'],
   plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
@@ -17,10 +14,10 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    "prettier/prettier": "error",
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    "camelcase": "off",
-    "no-unused-vars": ["error", { "argsIngorePattern": "next" }]
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIngorePattern: 'next' }],
   },
 };
