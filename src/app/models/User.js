2350577d64fe48e74c9bddf215a3d454b735e0e4
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2019-12-17 21:39:35 -0300

    Add a virtual field in User model to save the hashed password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
-import { Model, STRING, BOOLEAN } from 'sequelize';
+import { Model, STRING, BOOLEAN, VIRTUAL } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -6,11 +7,19 @@ class User extends Model {
       {
         name: STRING,
         email: STRING,
+        password: VIRTUAL,
         password_hash: STRING,
         provider: BOOLEAN,
       },
       { sequelize }
     );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password)
+        user.password_hash = await bcrypt.hash(user.password, 8);
+    });
+
+    return this;
   }
 }
 
