c3fb6cc7bd8bcdad460dceeb42d392daa1363751
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2019-12-17 21:31:14 -0300

    UserController with user creation

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'youssef',
-    email: 'youssef@gmail.com',
-    password_hash: '13123',
-  });
-
-  return res.json(user);
-});
+routes.post('/user', UserController.store);
 
 export default routes;
