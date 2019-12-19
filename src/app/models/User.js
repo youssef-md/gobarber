7bdeb6936d4c0fd893a76a310ed8139ccc4e8405
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2019-12-18 22:51:38 -0300

    Add SessionController with user's token creation using jwt

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -21,6 +21,10 @@ class User extends Model {
 
     return this;
   }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
 }
 
 export default User;
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '89f8b9309e24acb688a59bea35050b03',
+  expiresIn: '7d',
+};
