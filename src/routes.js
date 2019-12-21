4e782ca79f477a5b69c07d25d0714f84a5f7410e
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2019-12-21 18:35:13 -0300

    Config multer to store a picture in the server

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,7 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
@@ -6,6 +9,7 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -13,5 +17,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  res.json({ ok: true });
+});
 
 export default routes;
