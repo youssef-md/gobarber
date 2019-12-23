693009edc1ec9699933ed5f13090d0f7d4059780
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2019-12-22 22:11:31 -0300

    Associate User model with File via avatar_id

--- src/database/index.js
+++ src/database/index.js
@@ -15,7 +15,9 @@ class Database {
   init() {
     this.connection = new Sequelize(dbConfig);
 
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
