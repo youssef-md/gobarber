2ac8cc6cc8328b1183d7140a46332332b0f420a4
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2019-12-25 00:31:49 -0300

    Add paging in the user appointments listing based on the page query param

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -8,6 +8,8 @@ import File from '../models/File';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       where: {
         user_id: req.userId,
@@ -15,6 +17,8 @@ class AppointmentController {
       },
       attributes: ['id', 'date'],
       order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
