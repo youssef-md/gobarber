038e76a6b1a4adc7ef9da804ea285a886b59e3b9
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2019-12-25 23:16:17 -0300

    ScheduleController index method to list all provider's appointments of the informed day

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController.js';
 import AppointmentController from './app/controllers/AppointmentController.js';
+import ScheduleController from './app/controllers/ScheduleController.js';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -27,5 +28,7 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 export default routes;
