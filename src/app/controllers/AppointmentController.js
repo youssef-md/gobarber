8ba76b67ae74e83a27abb20da0f12f9609cd231b
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2019-12-26 11:27:40 -0300

    Create a notification document in mongodb to show a new appointment to the provider

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,10 +1,12 @@
 import * as Yup from 'yup';
 
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -85,6 +87,20 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date: reqHourOnly,
+    });
+
+    // Notify appointment to the provider
+    const user = await User.findByPk(req.userId);
+
+    const formattedDate = format(
+      reqHourOnly,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
+      provider: provider_id,
     });
 
     return res.json(appointment);
