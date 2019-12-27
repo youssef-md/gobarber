36ffe3243a6caf93f6dad307294fadf3c25d1df5
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2019-12-27 17:19:22 -0300

    Send a cancelation email to the provider with html template

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -153,7 +153,14 @@ class AppointmentController {
     await Nodemailer.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: `${appointment.user.name} cancelou o agendamento`,
-      text: 'Você tem um novo cancelamento...',
+      template: 'cancelation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
