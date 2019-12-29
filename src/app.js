5a6f98d7bfb68c6ce5117d012c172b0c7e0460bf
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2019-12-29 15:45:00 -0300

    Integrate with Sentry

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
 import express from 'express';
-import routes from './routes';
 import path from 'path';
+import 'express-async-errors';
+import * as Sentry from '@sentry/node';
+
+import routes from './routes';
 import './database'; // create sequelize connection and load all models
+
+import sentryConfig from './config/sentry';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
 
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -21,6 +29,7 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://049523b82a004332b92f0eb5e420cf64@sentry.io/1868307',
+};
