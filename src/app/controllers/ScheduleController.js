b1fbf476ff7e1491659961f22765033fbf96bcc5
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2019-12-30 15:16:38 -0300

    Return the name of the user that a provider has an appointment

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -27,6 +27,13 @@ class ScheduleController {
         },
       },
       order: ['date'],
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
     });
 
     return res.json(appointments);
