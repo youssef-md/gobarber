931b6bfb8dd3a77d43c6fce03ce946afae915257
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2019-12-30 15:02:39 -0300

    Allow CORS in dev mode without setting access address

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import Youch from 'youch';
 import 'express-async-errors';
 import * as Sentry from '@sentry/node';
@@ -23,6 +24,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
