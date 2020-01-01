decd05f6b6b8bba0d2b10692ca5a20f29d616b19
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2020-01-01 19:16:37 -0300

    Configure Redux, Redux Saga and Reactotron with both

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import Routes from './routes';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
 
-import { Router } from 'react-router-dom';
+import Routes from './routes';
 import history from './services/history';
+
+import store from './store';
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
 
