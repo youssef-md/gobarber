768c04c8340e20d970157738d7e635847df47691
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2020-01-01 17:25:57 -0300

    Style for the AuthLayout and create LogIn page

--- frontend/src/pages/LogIn/index.js
+++ frontend/src/pages/LogIn/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+import logo from '../../assets/logo.svg';
 // import { Container } from './styles';
 
 export default function LogIn() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <img src={logo} alt="Go Barber logo" />
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar sua conta gratuita</Link>
+      </form>
+    </>
+  );
 }
