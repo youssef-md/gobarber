ba64db0858ee78b6776d6d91721a96bf55439d7c
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2019-12-31 21:54:45 -0300

    Configure ESLint and Prettier to automate styleguide formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Chavoso</h1>
+        <h1>
+          Chaasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssvoso
+        </h1>
       </header>
     </div>
   );
