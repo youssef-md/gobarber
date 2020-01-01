752edd797860ad024ee3fbc1ed83c38869d7436e
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2020-01-01 17:39:27 -0300

    Use unform to easily retrieve form data

--- frontend/src/pages/LogIn/index.js
+++ frontend/src/pages/LogIn/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '../../assets/logo.svg';
 
 export default function LogIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Go Barber logo" />
-      <form>
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
 
         <button type="submit">Acessar</button>
         <Link to="/register">Criar sua conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
