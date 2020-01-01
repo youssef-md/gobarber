6b541b66f449db003bab986c43c91b9bb6fb6644
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2020-01-01 15:25:44 -0300

    Create Route guard component with proptypes validation

---
+++ frontend/src/routes/Route.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const logged = true;
+
+  if (!logged && isPrivate) return <Redirect to="/" />;
+
+  if (logged && !isPrivate) return <Redirect to="/dashboard" />;
+
+  return <Route {...rest} component={Component} />;
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
