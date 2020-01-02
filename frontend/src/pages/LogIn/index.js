fbe8cba804ea225b5f1f4c6701773584cbfede97
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2020-01-02 19:16:20 -0300

    Dispatch action to call saga to create user session and redirect him

--- frontend/src/pages/LogIn/index.js
+++ frontend/src/pages/LogIn/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
-
+import { useDispatch } from 'react-redux';
 import * as Yup from 'yup';
+
+import { signInRequest } from '../../store/modules/auth/actions';
 
 import logo from '../../assets/logo.svg';
 
@@ -16,8 +18,10 @@ const schema = Yup.object().shape({
 });
 
 export default function LogIn() {
-  function handleSubmit(data) {
-    console.tron.log(data);
+  const dispatch = useDispatch();
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
   }
 
   return (
