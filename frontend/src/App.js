757995ab6b21eb5018a630c9ca78327ef56ca6db
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2020-01-02 19:50:18 -0300

    Persist auth and user reducer to storage and rehydrate them

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 import history from './services/history';
 
-import store from './store';
+import { store, persistor } from './store';
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
