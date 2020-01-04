18cec7d525a438ca7e2cc7db4ce67f1cea4bd046
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2020-01-04 13:56:58 -0300

    Header component in default layout

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Container, Content, Profile } from './styles';
+import logo from '../../assets/logo-purple.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} al="Go Barber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          Notificação
+          <Profile>
+            <div>
+              <strong>Youssef Muhamad</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/126/abott@adorable.png"
+              alt="Foto de Youssef Muhamad"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
