c365b1ec7429064344939b89a82eecf99dff3788
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2020-01-06 16:04:53 -0300

    Layout for the provider's dashboard

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import api from '../../services/api';
-// import { Container } from './styles';
+
+import { Container, Time } from './styles';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 export default function Dashboard() {
-  api.get('appointments');
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#FFF" />
+        </button>
+        <strong>06 de Janeiro</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#FFF" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+
+        <Time>
+          <strong>09:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+
+        <Time>
+          <strong>10:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+
+        <Time available>
+          <strong>11:00</strong>
+          <span>Em aberto</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
