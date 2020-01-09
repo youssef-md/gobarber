f67bd131ec0829cdc4d43fec286c6eaa2fe61546
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2020-01-08 23:28:14 -0300

    Config statusbar

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 import Routes from './routes';
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  );
 }
 
 export default App;
