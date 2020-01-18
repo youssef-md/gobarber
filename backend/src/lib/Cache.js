52ac122c352dddce5a3fd299548813071b95568f
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2020-01-18 19:03:10 -0300

    Connect a cache to Redis and create the set, get and invalidate methods

---
+++ backend/src/lib/Cache.js
@@ -0,0 +1,27 @@
+import Redis from 'ioredis';
+
+class Cache {
+  constructor() {
+    this.redis = new Redis({
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      keyPrefix: 'cache:',
+    });
+  }
+
+  set(key, value) {
+    // expires in 24h to invalidate this cache to prevent that a dependency will be never updated
+    return this.redis.set(key, JSON.stringify(value), 'EX', 60 * 60 * 24);
+  }
+
+  async get(key) {
+    const cached = await this.redis.get(key);
+    return cached ? JSON.parse(cached) : null;
+  }
+
+  invalidate(key) {
+    return this.redis.del(key);
+  }
+}
+
+export default new Cache();
