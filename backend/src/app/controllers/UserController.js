4af051a55eba748506c8c441a8870442a57c30d9
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2020-01-17 21:56:53 -0300

    Create validator middleware for the User store method

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,23 +1,8 @@
 import User from '../models/User';
 import File from '../models/File';
 
-import * as Yup from 'yup';
-
 class UserController {
   async store(req, res) {
-    const schema = Yup.object().shape({
-      name: Yup.string().required(),
-      email: Yup.string()
-        .email()
-        .required(),
-      password: Yup.string()
-        .required()
-        .min(6),
-    });
-
-    if (!(await schema.isValid(req.body)))
-      return res.status(400).json({ error: 'Validation fails!' });
-
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists)
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -12,12 +12,14 @@ import ScheduleController from './app/controllers/ScheduleController.js';
 import NotificationController from './app/controllers/NotificationController.js';
 import AvailableController from './app/controllers/AvailableController';
 
+import validateUserStore from './app/validators/UserStore';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.post('/users', UserController.store);
+routes.post('/users', validateUserStore, UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware); // Global token validator
