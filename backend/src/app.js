2ef3be8e02baa67b74370461eda9051d7d54d4f6
youssef muhamad <emaildeyoussefmuhamad@gmail.com>
2020-01-21 12:32:50 -0300

    Config global route rate limit

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,9 @@ import Youch from 'youch';
 import 'express-async-errors';
 import * as Sentry from '@sentry/node';
 import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 
 import routes from './routes';
 import './database'; // create sequelize connection and load all models
@@ -32,6 +35,20 @@ class App {
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     );
     this.server.use(helmet());
+    // if (process.env.NODE_ENV !== 'development') {
+    this.server.use(
+      new RateLimit({
+        store: new RateLimitRedis({
+          client: redis.createClient({
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT,
+          }),
+        }),
+        windowMs: 1000 * 60 * 15,
+        max: 20,
+      })
+    );
+    // }
   }
 
   routes() {
