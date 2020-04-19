376c628e543d70d0013e6e6215b95c8354db5a79
Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
2020-04-18 22:01:20 -0300

    Config vscode debugger attach mode

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,6 +2,10 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (req, res) => res.json({ ok: false }));
+routes.get('/', (req, res) => {
+  const user = { name: 'youssef', email: 'youssef@gmail.com' };
+
+  return res.json(user);
+});
 
 export default routes;
