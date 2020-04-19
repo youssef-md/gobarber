45b48e8bc83f332fc67b766c7701d0ddd454be55
Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
2020-04-18 21:36:50 -0300

    Config eslint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'asd!'});
-})
+app.get('/', (req, res) => res.json({ message: 'asd!' }));
 
 app.listen(3333, () => {
-  console.log('Hello World!')
-})
+  console.log('Hello World!');
+});
