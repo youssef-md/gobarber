39b7679b88cdbd572a88eded84f28d4a42995e4e
Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
2020-04-20 23:38:02 -0300

    Add service to create an appointment with same date validation

---
+++ backend/src/service/CreateAppointmentService.ts
@@ -0,0 +1,36 @@
+import { getCustomRepository } from 'typeorm';
+import { startOfHour } from 'date-fns';
+
+import AppointmentsReposity from '../repositories/AppointmentsRepository';
+import Appointment from '../models/Appointment';
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({ provider, date }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsReposity);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate)
+      throw Error('This appointment is already booked!');
+
+    const appointment = appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
