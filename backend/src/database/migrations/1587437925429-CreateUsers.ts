cac888fbf4e5d6ce48996914128ad68b7d02cbc6
Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
2020-04-21 21:26:32 -0300

    Create Appointments - Users relationship considering the provider

--- backend/src/database/migrations/1587437925429-CreateUsers.ts
+++ backend/src/database/migrations/1587437925429-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1587437925429 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
