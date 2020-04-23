c4832547e2825cbb634673598952bbd758bb179f
Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
2020-04-22 23:14:53 -0300

    Serve all /tmp files statically

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,11 +3,14 @@ import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
 
+import uploadConfig from './config/upload';
 import './database';
 
 const app = express();
+
 app.use(express.json());
 app.use(routes);
+app.use('/files', express.static(uploadConfig.directory));
 
 app.listen(3333, () => {
   console.log('Hello World!');
