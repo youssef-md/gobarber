d91a3e54b1c91e5cf7b3375693b540e2198aa834
Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
2020-04-27 23:52:46 -0300

    Add global styles with styled-components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => <h1>Oie!</h1>;
+const App: React.FC = () => (
+  <>
+    <h1>Oieee!</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
