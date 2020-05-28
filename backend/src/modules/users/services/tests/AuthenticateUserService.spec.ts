5654dee031761cc026088688647b7a355b1f7501
Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
2020-05-28 20:10:15 -0300

    Test AuthenticateUserService with a mocked HashProvider and UserRepository

--- backend/src/modules/users/services/tests/AuthenticateUserService.spec.ts
+++ backend/src/modules/users/services/tests/AuthenticateUserService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../../repositories/fakes/FakeUsersRepository';
 import AuthenticateUserService from '../AuthenticateUserService';
 import CreateUserService from '../CreateUserService';
@@ -30,5 +31,52 @@ describe('AuthenticateUser', () => {
 
     expect(user).toEqual(userPayload);
     expect(token).toBeTruthy();
+  });
+
+  it('should not authorize invalid email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: 'error@gmail.com',
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not authorize user with the wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const userEmail = 'youssef@gmail.com';
+
+    await createUser.execute({
+      name: 'Youssef',
+      email: userEmail,
+      password: '123123',
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: userEmail,
+        password: '000000',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
