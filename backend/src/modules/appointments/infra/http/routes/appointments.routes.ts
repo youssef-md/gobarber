b1477e8f05049054fbf20a095fd32ff35eb404c6
Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
2020-06-16 22:09:31 -0300

    List provider schedule based on the sent date

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,12 +2,16 @@ import { Router } from 'express';
 
 import ensureAuth from '@modules/users/infra/http/middlewares/ensureAuth';
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuth);
 
 appointmentsRouter.post('/', appointmentsController.create);
+
+appointmentsRouter.get('/schedule', providerAppointmentsController.index);
 
 export default appointmentsRouter;
---
+++ backend/src/modules/appointments/services/tests/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,37 @@
+import FakeAppointmentsRepository from '../../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from '../ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(function instantiateAllTheDependencies() {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should list all the daily appointments on a date', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider_id',
+      user_id: 'user_id',
+      date: new Date(2020, 4, 20, 14, 14, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider_id',
+      user_id: 'user_id',
+      date: new Date(2020, 4, 20, 14, 15, 0, 0),
+    });
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'provider_id',
+      day: 20,
+      month: 5,
+      year: 2020,
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
