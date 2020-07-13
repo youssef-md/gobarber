42e25fd9ba0e7cba8bd1eef74c96a5da8cae0887
Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
2020-07-12 23:02:07 -0300

    Send date via query not via body

--- backend/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
+++ backend/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -6,7 +6,7 @@ import ListProviderMonthAvailability from '@modules/appointments/services/ListPr
 export default class ProviderMonthAvailabilityController {
   public async index(req: Request, res: Response): Promise<Response> {
     const { provider_id } = req.params;
-    const { month, year } = req.body;
+    const { month, year } = req.query;
 
     const listProviderMonthAvailability = container.resolve(
       ListProviderMonthAvailability,
@@ -14,8 +14,8 @@ export default class ProviderMonthAvailabilityController {
 
     const availability = await listProviderMonthAvailability.execute({
       provider_id,
-      month,
-      year,
+      month: Number(month),
+      year: Number(year),
     });
 
     return res.json(availability);
